66d9918088241a5da0c832a0c8e63f0f3e4c4258
Ramazan Ay <Ramazan_Ay@epam.com>
2024-11-01 21:31:00 +0100

    Create Catalog API project folders and .sln

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container
+
+
+var app = builder.Build();
+
+
+//Configure HTTP request pipeline
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
