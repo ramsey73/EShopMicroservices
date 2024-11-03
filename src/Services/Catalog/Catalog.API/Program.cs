801981d9b66cf78a3c0fe49d784c04dee95caee9
Ramazan Ay <Ramazan_Ay@epam.com>
2024-11-03 12:35:43 +0100

    Add docker-compose and CQRS

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    //config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 
 var app = builder.Build();
 
-
-//Configure HTTP request pipeline
-
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
